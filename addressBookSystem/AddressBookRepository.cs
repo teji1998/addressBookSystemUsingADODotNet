b77f81a637abc20a1eae878a8afde9ac8a8841a2
“teji1998” <tejasvinirpk@gmail.com>
2020-12-17 17:30:05 +0530

    Added:Established a connection between ADO dotnet and database

--- addressBookSystem/AddressBookRepository.cs
+++ addressBookSystem/AddressBookRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace addressBookSystem
 {
     public class AddressBookRepository
     {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book_Service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
     }
 }
