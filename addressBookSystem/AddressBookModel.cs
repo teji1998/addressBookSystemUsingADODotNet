41611536b9ce2dc12e297918f6cc98d1efb3d402
“teji1998” <tejasvinirpk@gmail.com>
2020-12-17 18:19:13 +0530

    Added:POCO class for addressbook

--- addressBookSystem/AddressBookModel.cs
+++ addressBookSystem/AddressBookModel.cs
@@ -6,5 +6,18 @@ namespace addressBookSystem
 {
     public class AddressBookModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public string MobileNumber{ get; set; }
+        public string EmailId { get; set; }
+        public string AddressBookName { get; set; }
+        public string Type { get; set; }
+
+
+
     }
 }
